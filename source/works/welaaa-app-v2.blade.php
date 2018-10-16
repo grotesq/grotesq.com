15d6cff1915e72280f5f785a2d2303c047d1776a
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2018-10-16 19:24:39 +0900

    윌라 웹 서비스 페이지 추가

---
+++ source/works/welaaa-app-v2.blade.php
@@ -0,0 +1,17 @@
+@php
+    $activeType = 'works';
+@endphp
+
+@extends('_layouts.master')
+
+@section('body')
+    <div class="gq-section gq-section--dark">
+        <h1><span>Welaaa App v2</span></h1>
+
+        <div class="container">
+            <p class="mt-5">
+                (주)인플루엔셜의 스마트러닝 서비스인 `윌라`의 앱을 리뉴얼했습니다.
+            </p>
+        </div>
+    </div>
+@endsection
