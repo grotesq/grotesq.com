265d572d671a41b374c461214a219bb6039e3da2
jiyeon2 <jiyeon.kim@grotesq.com>
2022-02-04 13:52:21 +0900

    추가 : 메타 태그 이미지, 설명 추가

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -11,6 +11,17 @@ export default function Works() {
     <BaseLayout>
       <Head>
         <title>포트폴리오 :: 그로테스큐 GrotesQ</title>
+        <meta property="og:title" content="포트폴리오 :: 그로테스큐 GrotesQ" />
+        <meta
+          property="og:description"
+          content="그로테스큐 스튜디오의 주요 프로젝트 입니다.
+                  완벽한 서비스를 제공하기 위해서 항상 신기술을 연구하고, 견고한 내부 인프라를 구축해왔습니다."
+        />
+        <meta
+          name="description"
+          content="그로테스큐 스튜디오의 주요 프로젝트 입니다.
+                  완벽한 서비스를 제공하기 위해서 항상 신기술을 연구하고, 견고한 내부 인프라를 구축해왔습니다."
+        />
       </Head>
       <main>
         <Carousel />
